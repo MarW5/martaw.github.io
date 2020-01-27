70611bf93d0895a96a091351b21d076e527e247c
MarW5 <marta.wiese5@gmail.com>
2020-01-27 14:09:35 +0100

    Give things back

---
+++ build/precache-manifest.ad459e3c98f7e304b04eaa0a4c4c7726.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddffb6050d9a3eafb734a640b152da7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "744c0a400a89954e9af8",
+    "url": "/static/css/main.753e2a06.chunk.css"
+  },
+  {
+    "revision": "e881093564816c376dd4",
+    "url": "/static/js/2.8c7f0a4e.chunk.js"
+  },
+  {
+    "revision": "c87d96dbcdc8dd1df6c21457f4947682",
+    "url": "/static/js/2.8c7f0a4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "744c0a400a89954e9af8",
+    "url": "/static/js/main.adfa6a1a.chunk.js"
+  },
+  {
+    "revision": "eb4cd7f87729f3b2dc23",
+    "url": "/static/js/runtime-main.212eab87.js"
+  },
+  {
+    "revision": "7db36abd712c228ea96e6206440a9eb3",
+    "url": "/static/media/3Columns_Background.7db36abd.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "aab5aa4cf97893341cd5aed84c736cd5",
+    "url": "/static/media/Background-Form.aab5aa4c.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "3bee335ab47afd8b714411f4841f0185",
+    "url": "/static/media/Form_Hero_Image.3bee335a.jpg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "c470ec0c56e643f8ee1898ea962e753a",
+    "url": "/static/media/Icon-Arrow-Down.c470ec0c.svg"
+  },
+  {
+    "revision": "9b7f6ef4fe770eee646ea281f111b15d",
+    "url": "/static/media/Icon-Arrow-Up.9b7f6ef4.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/static/media/Signature.e54cfcfc.svg"
+  }
+]);
