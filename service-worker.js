68a25012e639b4f0a1c6ba344f0b4d1274ff923c
MarW5 <marta.wiese5@gmail.com>
2020-01-27 15:29:10 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/martaw.github.io/precache-manifest.ef7de58b15e587972eb54287731f28c1.js"
+  "/martaw.github.io/precache-manifest.b10710d6c1aa6512fa29c1b93c0c050e.js"
 );
 
 self.addEventListener('message', (event) => {
